d4d58af29a59fe35842b1a21b12506a66b4a9cbc
Alexis Newby <alexis.newby@gmail.com>
2021-10-25 08:56:33 -0600

    Refactor hobbit

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,24 +1,53 @@
+// class Hobbit {
+//   constructor({name}) {
+//     this.name = name;
+//     this.age = 0;
+//     this.adult = false;
+//     this.old = false;
+//     this.hasRing = false;
+//   }
+//   celebrateBirthday () {
+//       this.age = this.age + 1
+//       if (this.age >= 33) {
+//         this.adult = true;
+//       }
+//       if (this.age >= 101) {
+//         this.old = true;
+//       }
+//     }
+//   getRing () {
+//     if (this.name === 'Frodo') {
+//       this.hasRing = true;
+//       return 'Here is the ring!';
+//     } else {
+//       return 'You can\'t have it!'
+//     }
+//   }
+// }
+
 class Hobbit {
-  constructor({name}) {
+  constructor({name}, adult, old) {
     this.name = name;
     this.age = 0;
-    this.adult = false;
-    this.old = false;
-    this.hasRing = false;
+    this.adult = false
+    this.old = false
+    this.hasRing = false
   }
-  celebrateBirthday () {
-      this.age = this.age + 1
-      if (this.age >= 33) {
-        this.adult = true;
-      }
-      if (this.age >= 101) {
-        this.old = true;
-      }
+
+  celebrateBirthday() {
+    this.age +=1
+    if (this.age >=33) {
+      this.adult = true;
     }
-  getRing () {
+    if (this.age >= 101) {
+      this.old = true
+    }
+  }
+
+  getRing() {
     if (this.name === 'Frodo') {
       this.hasRing = true;
-      return 'Here is the ring!';
+      return 'Here is the ring!'
     } else {
       return 'You can\'t have it!'
     }
