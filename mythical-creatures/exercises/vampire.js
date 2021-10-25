db870ce768087c31596c44734e8ec3763ffac48a
Alexis Newby <alexis.newby@gmail.com>
2021-10-25 08:34:21 -0600

    Refactor vampire

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,22 +1,44 @@
+// class Vampire {
+//   constructor (name, pet) {
+//     this.name = name;
+//     if (pet === undefined) {
+//       this.pet = 'bat';
+//     } else {
+//       this.pet = pet;
+//     }
+//     this.thirsty = true;
+//     this.counter = 0;
+//     this.ouncesDrank = 0;
+//   }
+//   drink() {
+//     this.counter = this.counter + 1;
+//     if (this.counter >= 1) {
+//       this.thirsty = false;
+//     }
+//     this.ouncesDrank = this.ouncesDrank + 10;
+//     if (this.counter > 5) {
+//       return "I'm too full to drink anymore!"
+//     }
+//   }
+// }
+
 class Vampire {
-  constructor (name, pet) {
+  constructor(name, pet) {
     this.name = name;
-    if (pet === undefined) {
-      this.pet = 'bat';
-    } else {
+    this.thirsty = true
+    this.ouncesDrank = 0
+    if (pet) {
       this.pet = pet;
+    } else {
+      this.pet = 'bat'
     }
-    this.thirsty = true;
-    this.counter = 0;
-    this.ouncesDrank = 0;
   }
   drink() {
-    this.counter = this.counter + 1;
-    if (this.counter >= 1) {
-      this.thirsty = false;
+    this.thirsty = false
+    if (this.ouncesDrank <= 50) {
+      this.ouncesDrank +=10
     }
-    this.ouncesDrank = this.ouncesDrank + 10;
-    if (this.counter > 5) {
+    if (this.ouncesDrank >= 50) {
       return "I'm too full to drink anymore!"
     }
   }
