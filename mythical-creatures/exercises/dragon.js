aa6d0df6ff854a4baae6a2c59ceb4af08ab368ce
Alexis Newby <alexis.newby@gmail.com>
2021-10-25 08:42:22 -0600

    Refactor dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,21 +1,39 @@
+// class Dragon {
+//   constructor (name, rider, counter) {
+//     this.name = name;
+//     this.rider = rider;
+//     this.hungry = true;
+//     this.counter = 0;
+//   }
+//   greet() {
+//     return `Hi, ${this.rider}!`;
+//   }
+//   eat() {
+//     this.counter = this.counter + 1;
+//     if (this.counter >= 3) {
+//       this.hungry = false;
+//     }
+//   }
+// }
+
+
+
 class Dragon {
-  constructor (name, rider, counter) {
+  constructor(name, rider) {
     this.name = name;
     this.rider = rider;
     this.hungry = true;
-    this.counter = 0;
+    this.hungerCounter = 0
   }
   greet() {
-    return `Hi, ${this.rider}!`;
+    return `Hi, ${this.rider}!`
   }
   eat() {
-    this.counter = this.counter + 1;
-    if (this.counter >= 3) {
-      this.hungry = false;
+    this.hungerCounter++
+    if (this.hungerCounter >= 3) {
+      this.hungry = false
     }
   }
 }
-
-
 
 module.exports = Dragon;
