14ff06fb1613977116e9caad07c075dd1d2d145e
Alexis Newby <alexis.newby@gmail.com>
2021-10-25 08:16:59 -0600

    Refactor unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,22 +1,43 @@
+// class Unicorn {
+//   constructor(name, color) {
+//     this.name = name;
+//     if (color === undefined) {
+//       this.color = 'white';
+//     } else {
+//       this.color = color;
+//     }
+//   }
+//   isWhite() {
+//     if (this.color !== 'white') {
+//       return false;
+//     }
+//   }
+//   says(message) {
+//     return `**;* ${message} *;**`;
+//   }
+//
+// }
+//
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    if (color === undefined) {
-      this.color = 'white';
-    } else {
+    if (color) {
       this.color = color;
+    } else {
+      this.color = 'white';
     }
-  }
-  isWhite() {
-    if (this.color !== 'white') {
-      return false;
-    }
-  }
-  says(message) {
-    return `**;* ${message} *;**`;
   }
 
+  isWhite() {
+    if (this.color !== 'white')
+    return false
+  }
+
+  says(message) {
+    return `**;* ${message} *;**`
+  }
 }
+
 
 module.exports = Unicorn;
 
