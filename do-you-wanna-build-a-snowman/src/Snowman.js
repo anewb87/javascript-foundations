e46c6b7be3c57f76c6fba8bc166d1ac0eadd569f
Alexis Newby <alexis.newby@gmail.com>
2021-10-26 08:46:57 -0600

    Finished Snowman, started Human

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,16 @@
 class Snowman {
-
+  constructor(snowmanPieces) {
+    this.carrots = snowmanPieces.carrots;
+    this.coal = snowmanPieces.coal;
+    this.buttons = snowmanPieces.buttons;
+    this.snowballs = snowmanPieces.snowballs;
+    this.magicHat = false;
+  }
+  canWearMagicHat() {
+    if (this.coal >= 2 && this.buttons >= 5 && this.carrots >= 1 && this.snowballs >=2) {
+      this.magicHat = true;
+    }
+  }
 }
 
 module.exports = Snowman;
